e83c9ddc32251a9dadd5298a0ffdbabff7eba0fd
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-11-24 18:06:03 -0800

    Added vote_event api endpoint.

--- app.rb
+++ app.rb
@@ -18,11 +18,8 @@ class App < Grape::API
   format :json
   formatter :json, PrettyJSON
 
-  desc "Get a list of councillors"
   get :councillors do
-    Person.limit(20).map do |person|
-      person.slice :id, :name, :image
-    end
+    Person.limit(20)
   end
 
   namespace :councillors do
@@ -41,7 +38,6 @@ class App < Grape::API
     end
   end
 
-  desc "Get a list of bills"
   get :bills do
     Bill.where(from_organization_id: TORONTO_COUNCIL_ORG_ID).limit(20).map do |bill|
       {
@@ -53,6 +49,26 @@ class App < Grape::API
           event.slice :start_date, :result, :motion_text, :motion_classification
         end
       }
+    end
+  end
+
+  get :vote_events do
+    VoteEvent.limit(20)
+  end
+
+  namespace :vote_events do
+    route_param :id do
+      get do
+        VoteEvent.find(params[:id])
+      end
+
+      get :person_votes do
+        VoteEvent.find(params[:id]).person_votes.limit(20)
+      end
+
+      get :people do
+        VoteEvent.find(params[:id]).people.limit(20)
+      end
     end
   end
 
@@ -96,21 +112,22 @@ end
 
 class PersonVote < ActiveRecord::Base
   self.table_name = 'opencivicdata_personvote'
-  belongs_to :person
+  belongs_to :person, foreign_key: :voter_id
   belongs_to :vote_event
 end
 
 class Bill < ActiveRecord::Base
   self.table_name = 'opencivicdata_bill'
-  belongs_to :legislative_session
   has_many :vote_events
+  belongs_to :legislative_session
 end
 
 class VoteEvent < ActiveRecord::Base
   self.table_name = 'opencivicdata_voteevent'
+  has_many :person_votes
+  has_many :people, through: :person_votes
   belongs_to :bill
   belongs_to :legislative_session
-  has_many :person_votes
 end
 
 class LegislativeSession < ActiveRecord::Base
