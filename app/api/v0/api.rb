7cd16d9f55d443629748ea0c409cb4ed205617e3
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-12-07 21:41:41 -0800

    Converted last of models to entity display.

--- app/api/v0/api.rb
+++ app/api/v0/api.rb
@@ -30,6 +30,41 @@ module App
       end
     end
 
+    class Events < Grape::API
+      include Grape::Kaminari
+      resource :events do
+        get do
+          @events = paginate Event.all
+          present @events
+        end
+
+        route_param :id do
+          get do
+            Event.find_by_uuid(params[:id])
+          end
+        end
+      end
+
+    end
+
+    class Posts < Grape::API
+      include Grape::Kaminari
+      resource :posts do
+        get do
+          @posts = paginate Post.all
+          present @posts
+        end
+
+        route_param :id do
+          get do
+            @post = Post.find_by_uuid(params[:id])
+            present @post
+          end
+        end
+      end
+
+    end
+
     class People < Grape::API
       include Grape::Kaminari
       resource :people do
@@ -56,6 +91,52 @@ module App
 
     end
 
+    class Memberships < Grape::API
+      include Grape::Kaminari
+      resource :memberships do
+        get do
+          @memberships = paginate Membership.all
+          present @memberships
+        end
+
+        route_param :id do
+          get do
+            @member = Membership.find_by_uuid(params[:id])
+            present @member
+          end
+        end
+      end
+    end
+
+    class Bills < Grape::API
+      include Grape::Kaminari
+      resource :bills do
+        get do
+          @bills = paginate Bill.all
+          present @bills
+        end
+
+        route_param :id do
+          get do
+            @bill = Bill.find_by_uuid(params[:id])
+            present @bill
+          end
+
+          get :vote_events do
+            Bill.find_by_uuid(params[:id]).vote_events.limit(20)
+          end
+
+          get :councillors do
+            Bill.find_by_uuid(params[:id]).people.limit(20)
+          end
+
+          get :votes do
+            Bill.find_by_uuid(params[:id]).person_votes.limit(20)
+          end
+        end
+      end
+    end
+
     class Organizations < Grape::API
       include Grape::Kaminari
       resource :organizations do
@@ -72,6 +153,37 @@ module App
         end
       end
 
+    end
+
+    class Votes < Grape::API
+      include Grape::Kaminari
+      resource :votes do
+        get do
+          @votes = paginate VoteEvent.all
+          present @votes
+        end
+
+        route_param :id do
+          get do
+            @vote = VoteEvent.find_by_uuid(params[:id])
+            present @vote
+          end
+
+          get :votes do
+            person_votes = VoteEvent.find_by_uuid(params[:id]).person_votes
+            paginated = paginate_array(person_votes)
+
+            person_votes_response paginated
+          end
+
+          get :councillors do
+            people = VoteEvent.find_by_uuid(params[:id]).people
+            paginated = paginate_array(people)
+
+            people_response paginated
+          end
+        end
+      end
     end
 
   end
