f2da914eb705034d43aa46b6aac822fd45f0d4ed
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-21 18:03:10 -0800

    Use ActiveRecord #includes instead of #joins.
    See: http://tomdallimore.com/blog/includes-vs-joins-in-rails-when-and-where/

--- app/models/agenda_item.rb
+++ app/models/agenda_item.rb
@@ -1,6 +1,6 @@
 class AgendaItem < ActiveRecord::Base
   self.table_name = 'opencivicdata_eventagendaitem'
-  default_scope { joins(:event).order('opencivicdata_event.start_time').order('opencivicdata_eventagendaitem.order::int') }
+  default_scope { includes(:event).order('opencivicdata_event.start_time').order('opencivicdata_eventagendaitem.order::int') }
   belongs_to :event
   has_many :event_related_entities
 
